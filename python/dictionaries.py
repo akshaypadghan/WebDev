46b7251637f9c7a966ac25815e8c53ab959bfe6f
theEverlastingOne <akshpadghan@gmail.com>
2018-02-24 00:21:36 +0000

    latest

---
+++ python/dictionaries.py
@@ -0,0 +1,4 @@
+ages={"Alice":23, "Bob":42, "Candice":28}
+print(ages)
+ages["Me"]=22
+print(ages)
---
+++ python/loops.py
@@ -0,0 +1,6 @@
+for i in range(10):
+    print(i)
+print()
+names=["Alice", "Bob", "Candice", "Dan"]
+for name in names:
+    print(name)
